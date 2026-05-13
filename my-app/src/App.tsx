950aa2806a0035199737c53b1139827c40a936c9
Viktor Erlandsson <72042504+trallan@users.noreply.github.com>
2026-05-13 10:51:40 +0000

    Update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,9 @@ import randomData from './randomData.json'
 function App() {
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
-  const [users, setUsers] = useState(randomData.users)
+  const [data, setData] = useState(randomData)
+
+  const users = data.users
 
   function handleClick(){
   	setCount(count + 1)
@@ -20,14 +22,14 @@ function App() {
 	<div>
 	<h1>Hello !</h1>
 	<button onClick={handleClick}>Count: {count}</button>
-	<label for="name">Enter your name</label>
+	<label>Enter your name</label>
 	<input name="name" type="text" onChange={handleChange}></input>
 	<h3>Your name is: {name}</h3>
 
 	<button onClick={() => alert(`${name}, why did you press the button...`)}>Dont Press</button>
 	</div>
 	<div>
-		{users.map(item => <li key={item.id}>{item.name}</li>)}
+	{users.map((user) => (<li key={user.id}>{user.name} | {user.email} | {user.age} | {user.role}</li>))}
 	</div>
 	</>
   )
